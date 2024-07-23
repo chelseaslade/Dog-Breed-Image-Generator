f5809de8fc95a01fa6f4eb55a12e7bad939d2b10
chelseaslade <153530277+chelseaslade@users.noreply.github.com>
2024-07-23 13:35:16 -0230

    Notes added

--- src/components/BreedList.jsx
+++ src/components/BreedList.jsx
@@ -1,31 +1,46 @@
 import React, { useState, useEffect } from "react";
 
-const BreedList = ({ chooseBreed, chooseImgNum }) => {
+//Set variables for breed list (breed & number of images)
+const BreedList = ({ setBreed, setImgNum }) => {
   const [breeds, setBreeds] = useState([]);
 
+  //Define Fetch data from API function
   useEffect(() => {
     const fetchBreedList = async () => {
       const response = await fetch("https://dog.ceo/api/breeds/list/all");
       const data = await response.json();
       setBreeds(Object.keys(data.message));
     };
+    //Call function
     fetchBreedList();
   }, []);
 
+  //Form submission set-up
+  const submitForm = (e) => {
+    e.preventDefault();
+    const breed = e.target.breed.value;
+    const imgNum = e.target.imgNum.value;
+    setBreed(breed);
+    setImgNum(imgNum);
+  };
+
+  //HTML returned
   return (
     <>
       <div className="entryForm">
-        <h2>Select Breed:</h2>
-        <select name="breed">
-          {breeds.map((breed) => (
-            <option key={breed} value={breed}>
-              {breed}
-            </option>
-          ))}
-        </select>
-        <h2>Select Image Number:</h2>
-        <input type="number" min="1" max="100" defaultValue="1"></input>
-        <button type="submit">Select</button>
+        <form onSubmit={submitForm}>
+          <h2>Select Breed:</h2>
+          <select name="breed">
+            {breeds.map((breed) => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
+          <h2>Select Image Number:</h2>
+          <input type="number" min="1" max="100" defaultValue="1"></input>
+          <button type="submit">Select</button>
+        </form>
       </div>
     </>
   );
